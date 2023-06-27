499d81d75d109475222990e2ece921bbcc0be6c7
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2023-06-27 14:35:50 -0400

    Add database connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
-const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+const port = 5001;
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(
   "/graphql",
