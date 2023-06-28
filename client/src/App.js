73f16162a37ca59a628171da08a3899ed3628cc5
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2023-06-27 20:54:00 -0400

    client initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/assets/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
