774305061ffeaf4f5da4942fee8501ddad6e8ed4
Hemant Bhardwaj <bhardwajhemant84@gmail.com>
2023-06-28 15:17:51 -0400

    Add client UI functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
 import Header from "./components/assets/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Clients from "./components/assets/Clients";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http:localhost:5001/graphql",
+  cache: cache,
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
